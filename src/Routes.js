3ac34dc4fc80223dceac37338d3e053aa265d6d3
iamwarint <warintorn.j@live.ku.th>
2024-02-07 22:09:35 +0700

    Edit routes

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import {BrowserRouter, Route, Switch } from 'react-router-dom'
+
+
+const Routes = () => {
+
+    return (
+
+        <BrowserRouter basename={'/se'}>
+
+            <Switch>
+
+
+            </Switch>
+
+        </BrowserRouter>
+
+    )
+}
+
+export default Routes
